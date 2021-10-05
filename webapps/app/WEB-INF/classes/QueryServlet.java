b71812e0e0d1319c0387a77350405ef9d8869505
Philip-36 <vlademir35ibe@gmail.com>
2021-10-05 21:08:53 +0900

    change html to json

--- webapps/app/WEB-INF/classes/QueryServlet.java
+++ webapps/app/WEB-INF/classes/QueryServlet.java
@@ -3,6 +3,7 @@ import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+
 @WebServlet("/query")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
 public class QueryServlet extends HttpServlet {
     // The doGet() runs once per HTTP GET request to this servlet.
@@ -10,13 +11,11 @@ public class QueryServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // Set the MIME type for the response message
-        response.setContentType("text/html");
+        response.setContentType("text/json");
         // Get a output writer to write the response message into the network socket
         PrintWriter out = response.getWriter();
         // Print an HTML page as the output of the query
-        out.println("<html>");
-        out.println("<head><title>Query Response</title></head>");
-        out.println("<body>");
+
         try (
                 // Step 1: Allocate a database 'Connection' object
                 Connection conn = DriverManager.getConnection(
